3d73082dcb986856242171351850c6f75c41ea1b
KatrinaS813 <sungkatr@usc.edu>
2025-02-20 23:18:13 -0800

    done hw2

--- util.cpp
+++ util.cpp
@@ -15,6 +15,7 @@ std::string convToLower(std::string src)
     to a set of words based on the criteria given in the assignment **/
 std::set<std::string> parseStringToWords(string rawWords)
 {
+  //parse
   std::set<std::string> words; 
   std::string word; 
   for(auto c : rawWords){
